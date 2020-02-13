f41342537eae768f00c365642c0fff29363594f5
Richard Borges Brito <richardborgesb@hotmail.com>
2020-02-13 17:26:13 -0300

    13-02-2020

---
+++ gobarber/src/config/databases.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'Teste@@123',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true,
+    },
+};
